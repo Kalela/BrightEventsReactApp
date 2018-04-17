9af456238127cb45b6b49fb5fc67f82f21267e1f
Philip Kalela <PhilSkiiiwalker@Philips-MacBook-Pro.local>
2018-04-17 15:24:49 +0300

    [feature] User can view all events

--- src/Components/ViewEvents/Events.js
+++ src/Components/ViewEvents/Events.js
@@ -34,24 +34,49 @@ class Events extends Component {
   render(){
     return (
         <div className="container">
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/">BrightEvents</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">BrightEvents</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
               </ul>
-              <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search Events" aria-label="Search" />
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search Events" aria-label="Search" />
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
               </form>
             </div>
           </nav>
+          <div className="card text-center">
+          {
+            this.state.events.map((dynamicData,key) =>
+            <div>
+              <div className="card-header">
+                <ul className="nav nav-tabs card-header-tabs">
+                  <li className="nav-item">
+                    <a className="nav-link active" href="#">{dynamicData.eventname}</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">RSVPs</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">{dynamicData.eventname}</h5>
+                <p className="card-text">{dynamicData.location}</p>
+                <p className="card-text">{dynamicData.date}</p>
+                <p className="card-text">{dynamicData.category}</p>
+                <a href="#" className="btn btn-primary">View Event</a>
+                <a href="#" className="btn btn-danger">Send Rsvp</a>
+              </div>
+            </div>
+            )
+          }
+          </div>
         </div>
-         );
-
+    );
   }
 };
 
