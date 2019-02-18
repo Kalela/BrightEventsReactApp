50edc19c554ab95cf73062f488648eb5bfda40a4
Philip Kalela <33759757+Kalela@users.noreply.github.com>
2019-02-18 14:36:37 +0300

    Update CreateEvent.js

--- src/Components/CreateEvent/CreateEvent.js
+++ src/Components/CreateEvent/CreateEvent.js
@@ -47,7 +47,7 @@ class CreateEvent extends Component {
       date: this.refs.date.value,
       category: this.state.category,
     };
-    fetch('https://bright-events-api-.herokuapp.com/api/v2/events', {
+    fetch('https://brighteventsapinowlive.herokuapp.com/api/v2/events', {
       method: 'POST',
       headers: {
         Accept: 'application/json, text/plain, */*',
